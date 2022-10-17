83fb574b7dbe7646bf5ae94bbba1ae56d033948a
Burak ERDEMİROĞLU <35409987+burakerdemiroglu@users.noreply.github.com>
2022-10-17 21:57:21 +0300

    Add files via upload

---
+++ Taksimetre/Taksimetre.java
@@ -0,0 +1,25 @@
+import com.sun.org.apache.xpath.internal.objects.XString;
+
+import java.util.Scanner;
+
+public class Taksimetre {
+    public static void main(String[] args) {
+        double cost, openingFee = 10, km = 2.20;
+        int kmDegeri;
+
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.print("Mileage Value :");
+
+        kmDegeri = input.nextInt();
+
+        cost = openingFee + (km * kmDegeri);
+
+        cost = (cost < 20) ? 20 : cost;
+
+        System.out.print("Taxi Fare: " + cost);
+
+    }
+
+}
